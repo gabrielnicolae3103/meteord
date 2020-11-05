38fbd72ae8b311baedb5f8a0ba05d39d08b5c3b9
Gabi <gabriel.nicolae313@gmail.com>
2020-11-05 13:57:06 +0200

    Meteor 1.11.1

--- base/scripts/lib/install_meteor.sh
+++ base/scripts/lib/install_meteor.sh
@@ -1,3 +1,3 @@
 set -e
 
-curl -sL https://install.meteor.com/?release=1.8.1 | sed s/--progress-bar/-sL/g | /bin/sh
+curl -sL https://install.meteor.com/?release=1.11.1 | sed s/--progress-bar/-sL/g | /bin/sh
